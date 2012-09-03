1dc9132ac3a3e9fff2061d1e5d7196ef5afb3742
Jason Clark <riceboyler@outlook.com>
2012-09-03 07:59:30 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Packtrkr::Application.config.secret_token = '0c74dac12ef356c50a674ef90710429317fe232db88ef9509c70a9190e0f39bdaaccf1bab3da53d01127f6c32c2574b1bc3545db7d4b8dd4e3e4f039703771f3'
